b485688e78441faa3bf7ae4ba17edd640bca656f
VladislavGregor <114413651+VladislavGregor@users.noreply.github.com>
2023-10-19 22:40:57 +0300

    Communication in real time

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import { createServer } from 'http';
+
+import OrdersSocket from './orders.socket';
+
+
+require('dotenv').config()
+import 'reflect-metadata';
+
+import {
+   createExpressServer,
+   RoutingControllersOptions
+} from 'routing-controllers'
+
+const port = process.env.APP_PORT || 3000;
+
+const routingControllerOptions: RoutingControllersOptions = {
+   routePrefix: 'v1',
+   controllers: [`${__dirname}/modules/http/*.controller.*`],
+   validation: true,
+   classTransformer: true,
+   cors: true,
+   defaultErrorHandler: true
+}
+
+const app = createExpressServer(routingControllerOptions);
+
+const httpServer = createServer(app);
+const io = Websocket.getInstance(httpServer);
+
+io.initializeHandlers([
+   { path: '/orders', handler: new OrdersSocket() }
+]);
+
+
+httpServer.listen(port, () => {
+   console.log(`This is working in port ${port}`);
+});
+
+import Websocket from './websocket/websocket';
+
+
+httpServer.listen(port, () => {
+   console.log(`This is working in port ${port}`);
+});
